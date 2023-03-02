17994a591b59cd29c95ec07fe6c748106017cb86
izharj <eezhar@gmail.com>
2023-03-02 12:18:14 +0800

    able to decode jwt valid jwt token. deconstructed info to be used in future when creating an app admin route

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 import { useAppStore } from "../stores/appStore";
+import { useJwt } from "react-jwt";
+import useAuth from "../hooks/useAuth";
 const Home = () => {
-  return <div>Home</div>;
+  const { auth } = useAuth();
+  const { decodedToken, isExpired } = useJwt(auth.access);
+  console.log("decodedToken: ", decodedToken);
+
+  return (
+    <div>
+      {decodedToken && <div>Access token info : {decodedToken.is_Admin}</div>}
+    </div>
+  );
 };
 
 export default Home;
