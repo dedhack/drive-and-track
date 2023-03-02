0c635592b84acaefbce7e8212b8dd1b0a19cbdcc
izharj <eezhar@gmail.com>
2023-03-03 01:40:13 +0800

    removed old frontend folder

--- client/src/stores/appStore.jsx
+++
@@ -1,8 +0,0 @@
-import { create } from "zustand";
-
-export const useAppStore = create((set) => ({
-  accessToken: null,
-  refreshToken: null,
-  setAccessToken: (accessToken) => set({ accessToken }),
-  setRefreshToken: (refreshToken) => set({ refreshToken }),
-}));
