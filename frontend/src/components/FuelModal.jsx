9bc1d29c970e477cc76d0597be66b93ee6464383
izharj <eezhar@gmail.com>
2023-03-05 02:29:38 +0800

    rough template for fuel form

--- frontend/src/components/FuelModal.jsx
+++ frontend/src/components/FuelModal.jsx
@@ -5,24 +5,79 @@ import { Modal, Button } from "react-daisyui";
 const FuelModal = ({ visible, setVisible }) => {
   //   const [visible, setVisible] = useState(false);
 
-//   const toggleVisible = () => {
-//     setVisible(!visible);
-//   };
+  //   const toggleVisible = () => {
+  //     setVisible(!visible);
+  //   };
   return (
     <div className="font-sans">
       {/* <Button onClick={toggleVisible}>Open Modal</Button> */}
-      <Modal open={visible}>
-        <Modal.Header className="font-bold">
-          Congratulations random Interner user!
-        </Modal.Header>
+      <Modal className="relative" open={visible}>
+        <Button
+          className="absolute top-2 right-2"
+          onClick={() => setVisible(!visible)}
+        >
+          X
+        </Button>
+
+        <Modal.Header className="font-bold">Add Fuel Log</Modal.Header>
 
         <Modal.Body>
-          You've been selected for a chance to get one year of subscription to
-          use Wikipedia for free!
+          <div className="flex flex-col">
+            <div className="mt-10">
+              <label htmlFor="">Odometer</label>
+              <input
+                type="text"
+                placeholder=""
+                className="input input-bordered input-secondary w-full max-w-xs"
+              />
+            </div>
+            <div>
+              <label htmlFor="">price</label>
+              <input
+                type="text"
+                placeholder=""
+                className="input input-bordered input-secondary w-full max-w-xs"
+              />
+            </div>{" "}
+            <div>
+              <label htmlFor="">Fuel amount</label>
+              <input
+                type="text"
+                placeholder=""
+                className="input input-bordered input-secondary w-full max-w-xs"
+              />
+            </div>
+            <div>
+              <label htmlFor="">Fuel grade</label>
+              <input
+                type="text"
+                placeholder=""
+                className="input input-bordered input-secondary w-full max-w-xs"
+              />
+            </div>
+            <div>
+              <label htmlFor="">Full Tank?</label>
+              <input
+                type="text"
+                placeholder=""
+                className="input input-bordered input-secondary w-full max-w-xs"
+              />
+            </div>
+            <div>
+              <label htmlFor="">Location</label>
+              <input
+                type="text"
+                placeholder=""
+                className="input input-bordered input-secondary w-full max-w-xs"
+              />
+            </div>
+          </div>
         </Modal.Body>
 
         <Modal.Actions>
-          <Button onClick={() => setVisible(!visible)}>Yay!</Button>
+          <Button onClick={() => setVisible(!visible)}>Save</Button>
+
+          <Button onClick={() => setVisible(!visible)}>Close</Button>
         </Modal.Actions>
       </Modal>
     </div>
