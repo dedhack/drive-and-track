1f6d74a1d0c02e2530366a0019e698e9ce5d9487
izharj <eezhar@gmail.com>
2023-03-23 12:49:09 +0800

    connected local server to cloud DB elephant SQL

--- server/db.js
+++ server/db.js
@@ -1,12 +1,26 @@
+//////////////////// UNHIDE HERE TO RUN DB LOCALLY
 const Pool = require("pg").Pool;
 // require("dotenv").config(); // TODO: not necessary
 
+// original
+// const pool = new Pool({
+//   user: process.env.USER, // to .env?
+//   password: process.env.DB_PASS, // to .env?
+//   host: "localhost",
+//   port: 5432, // to .env?
+//   database: "proj4", // to modify
+// });
+////////////////////
+
+const pg = require("pg");
+
+const conString = process.env.ELEPHANT_SQL;
 const pool = new Pool({
-  user: process.env.USER, // to .env?
-  password: process.env.DB_PASS, // to .env?
-  host: "localhost",
+  user: process.env.ELE_USER, // to .env?
+  password: process.env.ELE_PASS, // to .env?
+  host: conString,
   port: 5432, // to .env?
-  database: "proj4", // to modify
+  database: process.env.ELE_USER, // to modify
 });
 
 module.exports = pool;
